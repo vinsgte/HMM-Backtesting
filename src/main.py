f3536bbfbcebcb4da012c02ac3397f61744a4e23
Vincent Gauthereau <vincentgte31@gmail.com>
2026-01-05 21:07:47 +0100

    Updating main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("ok")
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from hmmlearn import hmm
+from sklearn.preprocessing import StandardScaler
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+
+
